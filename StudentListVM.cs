553671acb733e82933d2dfcba6878e3dc49780b8
FatimahAl-khateeb <50541668+FatimahAl-khateeb@users.noreply.github.com>
2019-10-01 21:29:49 +0300

    Add files via upload

---
+++ StudentListVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GlobaDev.Models
+{
+    public class StudentListVM
+    {
+        public IEnumerable<DAL.Student> Students { get; set; }
+        public int PageSize { get; set; }
+        public string search { get; set; }
+    }
+}
